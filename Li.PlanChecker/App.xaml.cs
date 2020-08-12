f125ce2a2d0242eb13a55c2deb04cead67d797af
Ji Li <liji9467@gmail.com>
2020-08-12 09:51:55 -0700

    test

--- Li.PlanChecker/App.xaml.cs
+++ Li.PlanChecker/App.xaml.cs
@@ -16,7 +16,7 @@ namespace Li.PlanChecker
 {
     /// <summary>
     /// Interaction logic for App.xaml
-    /// </summary>
+    /// </summary> ////
     public partial class App
     {
         private IEventAggregator _ea;
